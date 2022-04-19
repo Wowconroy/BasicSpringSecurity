cecd4bf4d54f49b16d3613afcc6ead8d6fe0b825
Oleksii <shostak.ok@gmail.com>
2022-04-19 19:12:20 +0300

    update ApplicationSecurityConfig

--- src/main/java/com/example/hugespringsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/hugespringsecurity/security/ApplicationSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static com.example.hugespringsecurity.security.ApplicationUserPermission.COURSE_WRITE;
 import static com.example.hugespringsecurity.security.ApplicationUserRole.*;
@@ -36,9 +37,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login").permitAll()
-                .defaultSuccessUrl("/courses", true)
-                .and()
-                .rememberMe();
+                .defaultSuccessUrl("/courses", true);
+//                .and()
+//                .logout().logoutUrl("/logout")
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+//                .clearAuthentication(true)
+//                .invalidateHttpSession(true)
+//                .deleteCookies("JSESSIONID")
+//                .logoutSuccessUrl("/login");
     }
 
     @Override
